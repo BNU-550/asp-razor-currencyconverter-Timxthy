998b3bd2845c73e1b95998b85e8cf73a2a03fc43
Timxthy <Timxthy@Github.com>
2021-11-30 16:48:22 +0000

    Razor Movies

---
+++ RazorMovies/Data/RazorMoviesContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorMovies.Models;
+
+namespace RazorMovies.Database
+{
+    public class RazorMoviesContext : DbContext
+    {
+        public RazorMoviesContext (DbContextOptions<RazorMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorMovies.Models.Movie> Movie { get; set; }
+    }
+}
