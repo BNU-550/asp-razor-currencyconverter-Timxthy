49460074c23fd9f173fc9cc405a79dfd3fe9a1df
Timxthy <Timxthy@Github.com>
2021-11-30 17:00:24 +0000

    Movies

--- RazorMovies/Models/Movie.cs
+++ RazorMovies/Models/Movie.cs
@@ -3,14 +3,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RazorMovies.Models
 {
+    public enum Genres
+    {
+        Action, Comedy, Horror, Thriller, Fiction
+    }
+
     public class Movie
     {
+        [Key]
+
         public int ID { get; set; }
+        [StringLength(60)]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
+
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
         public decimal Price { get; set; }
     }
 }
