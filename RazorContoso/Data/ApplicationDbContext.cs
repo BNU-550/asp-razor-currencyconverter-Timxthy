f345b8b079a9211cc2ef06a7b18030c8b10196d2
Timxthy <Timxthy72@gmail.com>
2021-12-03 13:04:31 +0000

    Updates

---
+++ RazorContoso/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RazorContoso.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorContoso.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
---
+++ RazorContoso/Pages/Students/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorContoso.Data;
+using RazorContoso.Models;
+
+namespace RazorContoso.Pages.Students
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorContoso.Data.ApplicationDbContext _context;
+
+        public DeleteModel(RazorContoso.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students.FirstOrDefaultAsync(m => m.StudentID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students.FindAsync(id);
+
+            if (Student != null)
+            {
+                _context.Students.Remove(Student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
